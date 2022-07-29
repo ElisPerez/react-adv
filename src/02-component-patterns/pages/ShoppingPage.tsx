0f70d6a6d41474c5e5c8bbbcea15c5424ca3c425
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-07-28 20:45:41 -0500

    interface ProductInCart

--- src/02-component-patterns/pages/ShoppingPage.tsx
+++ src/02-component-patterns/pages/ShoppingPage.tsx
@@ -1,12 +1,29 @@
+import { useState } from 'react';
 import { ProductCard, ProductButtons, ProductImage, ProductTitle } from '../components';
+import { Product } from '../interfaces/interfaces';
+
 import '../styles/custom-styles.css';
-const product = {
+
+const product1 = {
   id: '1',
   title: 'Coffee Mug',
   img: './coffee-mug.png',
 };
 
+const product2 = {
+  id: '2',
+  title: 'Coffee Mug - Meme',
+  img: './coffee-mug2.png',
+};
+
+const products: Product[] = [product1, product2];
+
+interface ProductInCart extends Product {
+  count: number;
+}
+
 export const ShoppingPage = () => {
+  const [shoppingCart, setShoppingCart] = useState<{ [key: string]: ProductInCart }>({});
   return (
     <div>
       <h1>ShoppingPage</h1>
@@ -18,50 +35,31 @@ export const ShoppingPage = () => {
           flexWrap: 'wrap',
         }}
       >
-        {/* Compound Component pattern Version 1 */}
-        <ProductCard product={product} className='bg-dark text-white'>
-          <ProductCard.Image className='custom-image' style={{boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}} />
-          <ProductCard.Title
-            className='text-bold'
-            title={'Title front props'}
-            activeClass='active'
+        {products.map(product => (
+          <ProductCard key={product.id} product={product} className='bg-dark text-white'>
+            <ProductImage
+              className='custom-image'
+              style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
+            />
+            <ProductTitle className='text-bold' activeClass='active' />
+            <ProductButtons className='custom-buttons' />
+          </ProductCard>
+        ))}
+      </div>
+      <div className='shopping-cart'>
+        <ProductCard product={product2} className='bg-dark text-white' style={{ width: '100px' }}>
+          <ProductImage
+            className='custom-image'
+            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
           />
-          <ProductCard.Buttons className='custom-buttons' />
-        </ProductCard>
-
-        {/* Compound Component pattern Version 2 */}
-        <ProductCard product={product} className='bg-dark text-white'>
-          <ProductImage className='custom-image' style={{boxShadow: '10px 10px 10px rgba(0,0,0,0.2)'}} />
-          <ProductTitle className='text-bold' activeClass='active' />
           <ProductButtons className='custom-buttons' />
         </ProductCard>
-
-        <ProductCard
-          product={product}
-          style={{
-            backgroundColor: '#70D1F8',
-            color: 'brown',
-          }}
-        >
+        <ProductCard product={product1} className='bg-dark text-white' style={{ width: '100px' }}>
           <ProductImage
-            style={{
-              borderRadius: '20px',
-              boxShadow: '10px 10px 10px rgba(0,0,0,0.2)',
-              padding: '10px',
-              width: 'calc(100% - 20px)',
-            }}
+            className='custom-image'
+            style={{ boxShadow: '10px 10px 10px rgba(0,0,0,0.2)' }}
           />
-          <ProductTitle
-            style={{
-              fontWeight: 'bold',
-            }}
-          />
-          <ProductButtons
-            style={{
-              display: 'flex',
-              justifyContent: 'end',
-            }}
-          />
+          <ProductButtons className='custom-buttons' />
         </ProductCard>
       </div>
       <hr />
