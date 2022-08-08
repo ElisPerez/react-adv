de82b647d655763553252e38c8d72259c3c22c13
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-08-07 22:03:46 -0500

    select and checkbox

--- src/03-forms/pages/FormikComponents.tsx
+++ src/03-forms/pages/FormikComponents.tsx
@@ -12,6 +12,8 @@ export const FormikComponents = () => {
           firstName: '',
           lastName: '',
           email: '',
+          terms: false,
+          jobType: '',
         }}
         onSubmit={values => {
           console.log(values);
@@ -20,6 +22,13 @@ export const FormikComponents = () => {
           firstName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
           lastName: Yup.string().max(15, 'Must be 15 characters or less').required('Required'),
           email: Yup.string().email('Invalid email address').required('Required'),
+          // terms: Yup.boolean().isTrue('You must accept the terms and conditions').required('Required'), // this is the same as the below
+          terms: Yup.boolean()
+            .oneOf([true], 'You must accept the terms and conditions')
+            .required('Required'),
+          jobType: Yup.string()
+            .notOneOf(['it-jr'], 'This option is not allowed')
+            .required('Required'),
         })}
       >
         {formik => (
@@ -35,6 +44,23 @@ export const FormikComponents = () => {
             <label htmlFor='email'>Email</label>
             <Field name='email' type='email' />
             <ErrorMessage name='email' component='span' />
+
+            {/* Select */}
+            <label htmlFor='jobType'>Jobs types</label>
+            <Field name='jobType' as='select'>
+              <option value=''>Pick something</option>
+              <option value='developer'>Developer</option>
+              <option value='designer'>Designer</option>
+              <option value='it-senior'>IT Senior</option>
+              <option value='it-jr'>IT Jr.</option>
+            </Field>
+            <ErrorMessage name='jobType' component='span' />
+
+            <label>
+              <Field name='terms' type='checkbox' />
+              Terms and conditions
+            </label>
+            <ErrorMessage name='terms' component='span' />
 
             <button type='submit'>Submit</button>
           </Form>
