caf615a0b8c53627e8a462ceda96f2f83da7b847
Elis Antonio Perez <elisperezmusic@gmail.com>
2022-08-10 00:15:26 -0500

    validations

--- src/03-forms/pages/DynamicForm.tsx
+++ src/03-forms/pages/DynamicForm.tsx
@@ -1,12 +1,38 @@
 import { Form, Formik } from 'formik';
 import { MySelect, MyTextInput } from '../components';
 import formJson from '../data/custom-form.json';
+import * as Yup from 'yup';
 
 const initialValues: { [key: string]: any } = {};
+const requiredFields: { [key: string]: any } = {};
 
 for (const input of formJson) {
   initialValues[input.name] = input.value;
+  if (!input.validations) continue;
+
+  let schema = Yup.string();
+  for (const rule of input.validations) {
+    if (rule.type === 'required') {
+      schema = schema.required('Required');
+    }
+
+    if (rule.type === 'minLength') {
+      schema = schema.min(
+        (rule as any).value || 2,
+        `Must be at least ${(rule as any).value || 2} characters`
+      );
+    }
+
+    if (rule.type === 'email') {
+      schema = schema.email('Invalid email address');
+    }
+
+    requiredFields[input.name] = schema;
+  }
 }
+
+// const validationSchema = Yup.object().shape(requiredFields);
+const validationSchema = Yup.object({ ...requiredFields });
 
 export const DynamicForm = () => {
   return (
@@ -14,13 +40,14 @@ export const DynamicForm = () => {
       <h1>Dynamic Form</h1>
       <Formik
         initialValues={initialValues}
+        validationSchema={validationSchema}
         onSubmit={values => {
           console.log(values);
         }}
       >
         {formik => {
           return (
-            <Form>
+            <Form noValidate>
               {formJson.map(({ label, name, placeholder, type, options }) => {
                 if (type === 'input' || type === 'password' || type === 'email') {
                   return (
@@ -36,7 +63,7 @@ export const DynamicForm = () => {
                   return (
                     <MySelect key={name} label={label} name={name}>
                       <option value=''>Select an Option</option>
-                      {options?.map(({id, label}) => (
+                      {options?.map(({ id, label }) => (
                         <option key={id} value={id}>
                           {label}
                         </option>
